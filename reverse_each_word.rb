647a7c16aa3cb73a354f44d3b1128b78eb34b4c1
Pancho Labrador <plabradorjr@gmail.com>
2019-10-12 17:15:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   reverse_array = []
   new_array = string.split(" ")
-  new_array.each do |word|
+  new_array.collect do |word|
     reverse_array << word.reverse
   end
   reverse_array.join(" ")
